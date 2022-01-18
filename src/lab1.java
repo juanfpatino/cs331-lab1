8f8e48141075688b8e59003e813ff5d69fbd2287
Juan Patino <juan9patino9@gmail.com>
2022-01-17 21:07:32 -0500

    ConfigElevation()

--- src/lab1.java
+++ src/lab1.java
@@ -12,10 +12,32 @@ public class lab1 {
         BufferedImage terrain = ImageIO.read(new File(args[0]));
         File elevationFile = new File(args[1]);
         File pathFile = new File(args[2]);
-        BufferedImage out = ImageIO.read(new File(args[3]));
+        String outFileName = args[3];
 
-        
+        Double[][] elevation = new Double[395][500];
+        Scanner s = new Scanner(elevationFile);
 
+        ConfigElevation(elevation, s);
+
+    }
+
+    private static void ConfigElevation(Double[][] elevation, Scanner s) {
+        for(int i = 0; i < 500; i++){
+
+            String[] line = s.nextLine().split(" ");
+            for(int j = 0; j < 395; j++){
+
+                String valAsString = line[j].substring(0, 8);
+
+                //remove the e+02 at the end then multiply by 100
+
+                double val = Double.parseDouble(valAsString);//primative?
+
+                elevation[i][j] = val;
+
+            }
+
+        }
     }
 
 }
